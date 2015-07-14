d1ae4354ac5639ee605ba8e28206d8bb9ba532fd
bl <tony581@sina.com>
2015-07-14 10:50:04 +0800

    Initial Commit

---
+++ ALDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALDemo
+//
+//  Created by bl on 15/7/14.
+//  Copyright (c) 2015年 bl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALDemo
+//
+//  Created by bl on 15/7/14.
+//  Copyright (c) 2015年 bl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
