1587d79ebffc75bd5bad50e58a26b95667169cb4
bl <tony581@sina.com>
2015-07-14 11:39:36 +0800

    add two button for push

--- ALDemo/ViewController.m
+++ ALDemo/ViewController.m
@@ -9,19 +9,103 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UIButton *_staticModeBtn;
+    UIButton *_dynamicModeBtn;
+}
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _staticModeBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    _staticModeBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    [_staticModeBtn setTitle:@"StaticModeBtn" forState:UIControlStateNormal];
+    [_staticModeBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_staticModeBtn setBackgroundColor:[UIColor lightGrayColor]];
+    [self.view addSubview:_staticModeBtn];
+    [_staticModeBtn addTarget:self action:@selector(staticModeBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    _dynamicModeBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    _dynamicModeBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    [_dynamicModeBtn setTitle:@"DynamicModeBtn" forState:UIControlStateNormal];
+    [_dynamicModeBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_dynamicModeBtn setBackgroundColor:[UIColor lightGrayColor]];
+    [self.view addSubview:_dynamicModeBtn];
+    [_dynamicModeBtn addTarget:self action:@selector(dynamicModeBtnClick) forControlEvents:UIControlEventTouchUpInside];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)updateViewConstraints
+{
+    [super updateViewConstraints];
+    // 水平方向
+    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:_staticModeBtn
+                                                                  attribute:NSLayoutAttributeRight
+                                                                  relatedBy:NSLayoutRelationEqual
+                                                                     toItem:self.view
+                                                                  attribute:NSLayoutAttributeCenterX
+                                                                 multiplier:1.f constant:-10];
+    [self.view addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:_dynamicModeBtn
+                                              attribute:NSLayoutAttributeLeft
+                                              relatedBy:NSLayoutRelationEqual
+                                                 toItem:self.view
+                                              attribute:NSLayoutAttributeCenterX
+                                             multiplier:1.f constant:10];
+    [self.view addConstraint:constraint];
+
+    // y方向
+    constraint = [NSLayoutConstraint constraintWithItem:_dynamicModeBtn
+                                              attribute:NSLayoutAttributeCenterY
+                                              relatedBy:NSLayoutRelationEqual
+                                                 toItem:self.view
+                                              attribute:NSLayoutAttributeCenterY
+                                             multiplier:1.f constant:0];
+    [self.view addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:_dynamicModeBtn
+                                              attribute:NSLayoutAttributeCenterY
+                                              relatedBy:NSLayoutRelationEqual
+                                                 toItem:_staticModeBtn
+                                              attribute:NSLayoutAttributeCenterY
+                                             multiplier:1.f constant:0];
+    [self.view addConstraint:constraint];
+    
+    // 宽高
+    constraint = [NSLayoutConstraint constraintWithItem:_dynamicModeBtn
+                                              attribute:NSLayoutAttributeWidth
+                                              relatedBy:NSLayoutRelationEqual
+                                                 toItem:_staticModeBtn
+                                              attribute:NSLayoutAttributeWidth
+                                             multiplier:1.f constant:0];
+    [self.view addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:_dynamicModeBtn
+                                              attribute:NSLayoutAttributeHeight
+                                              relatedBy:NSLayoutRelationEqual
+                                                 toItem:_staticModeBtn
+                                              attribute:NSLayoutAttributeHeight
+                                             multiplier:1.f constant:0];
+    [self.view addConstraint:constraint];
+    
+}
+
+
+#pragma mark - 按钮点击
+- (void)staticModeBtnClick
+{
+    
+}
+
+- (void)dynamicModeBtnClick
+{
+    
 }
 
 @end
